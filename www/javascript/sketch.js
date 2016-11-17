f1ab8b9f1607954f1d1be536ba81332bb58ab300
Jesse Okeya <jesseokeya@Jesses-MacBook-Pro.local>
2016-11-17 14:35:20 -0500

    update

--- www/javascript/sketch.js
+++ www/javascript/sketch.js
@@ -1,8 +1,8 @@
 //Variables and arrays of game
-var flObjects = [ ]; var level;
+var flObjects = [ ]; var level; var button
 var canvas; var img; var concrete  = [ ];
 var flies = [ ]; var flyObjects = [ ];
-var scoreCount = 0; var flyNumber = 20;
+var scoreCount = 0; var flyNumber = 20; var xposFrog;
 //function preload to load assets into the game
 function preload(){
   img = loadImage("assets/frog.png");
@@ -23,6 +23,8 @@ function setup() {
 
   level = 1;
 
+  xposFrog = width/2
+
   for(var i = 0; i < flies.length; i++){
     flyObjects.push(new fly(flies[i], 50, random(40, width - 40), random(110, height - 250)));
   }
@@ -30,8 +32,6 @@ function setup() {
   for(var i = 0; i < 600; i++){
     flObjects.push(new floatingObjects(random(width),random(height)));
   }
-  //push frog object into index 0 of concrete
-  concrete[0] = new frog(img, 200, width/2, height - 120);
 
   //push in random fly objects into game
   for(var i = 0; i < flies.length; i++){
@@ -46,10 +46,6 @@ function setup() {
 
   loadJSON('all', gotData);
   console.log("JSON file loaded sucessfully ");
-
-  if(scoreCount === flyNumber){
-
-  }
 }
 
 function gotData(data){
@@ -60,15 +56,18 @@ function gotData(data){
     var score = scoreCount;
   }
 }
+function mouseDragged() {
+  xposFrog = mouseX;
+}
 
-function falling(frog, fly){
+function falling(frog, fly, x, y){
   //stroke(200, 52, 0, 130);
   noFill();
   strokeWeight(2);
   noStroke();
-  ellipse(width/2, height - 120, 200, 200);
+  ellipse(x, y, 200, 200);
 
-  var distance = dist(frog.x, frog.y, fly.x, fly.y);
+  var distance = dist(x, y, fly.x, fly.y);
   if(distance < 250){return true;}
   return false;
 }
@@ -80,7 +79,8 @@ function draw(){
   concrete[1] = new score("Score: ", 35, scoreCount, 14, 60);
   //push level object into index 1 of concrete
   concrete[2] = new score("Level", 35, level, width - 154, 60);
-
+  //push frog object into index 0 of concrete
+  concrete[0] = new frog(img, 200, xposFrog, height - 120);
   //display and move fireflies
   for(var i = 0; i < flObjects.length; i++){ flObjects[i].display(); flObjects[i].move();}
 
@@ -103,7 +103,7 @@ function draw(){
   }
   for(var i = 0; i < flies.length; i++){
     if(flyObjects[i].getId() === true && flyObjects[i].intersect() === true && scoreCount < flyNumber){
-      concrete[0].eat(width/2 - 3, height - 144, flyObjects[i].x, flyObjects[i].y);
+      concrete[0].eat(concrete[0].x, concrete[0].y - 24, flyObjects[i].x, flyObjects[i].y);
       flyObjects[i].y = flyObjects[i].y + 60;
       flyObjects[i].setExcecute(false);
     }
@@ -111,9 +111,9 @@ function draw(){
   strokeWeight(8);
   fill(200, 52, 0);
   stroke(200, 52, 0, 200);
-  ellipse(width/2 - 3, height - 144, 15, 15);
+  ellipse(concrete[0].x, concrete[0].y - 24, 15, 15);
 for(var i = 0; i < flies.length; i++){
-  if(falling(concrete[0], flyObjects[i]) === true){
+  if(falling(concrete[0], flyObjects[i], concrete[0].x, concrete[0].y) === true){
     flyObjects[i].x = flyObjects[i].x + 50;
     console.log("intersect");
     }
