d1a76cc8e99fe986322f33a3a54d31f780b74e01
Jesse Okeya <jesseokeya@Jesses-MacBook-Pro.local>
2016-12-04 15:14:48 -0500

    commit

--- www/javascript/extra.js
+++ www/javascript/extra.js
@@ -1,6 +1,10 @@
 function redirectPage(){
-  window.location.href = "https://froggy-game.herokuapp.com/delete";
-  setTimeout(function(){
-    window.location.href = "https://froggy-game.herokuapp.com";
-  }, 10);
+  var value = false;
+  if(value === false){
+    window.location.href = "https://froggy-game.herokuapp.com/delete";
+    setTimeout(function(){
+      window.location.href = "https://froggy-game.herokuapp.com";
+    }, 10);
+    value = true;
+  }
 }
